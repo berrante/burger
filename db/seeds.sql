692f2895d1c4dab606515db74961a1411163d71d
Bridget Errante <brierrante@gmail.com>
2017-09-11 20:22:15 -0400

    Adds seeds.sql content.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portobello Burger', FALSE);
